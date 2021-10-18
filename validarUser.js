018453f4655d5382a6937644d171b3f6fcc49e49
Fran <franbl98@gmail.com>
2021-10-18 14:19:30 +0200

    Clase 18 octubre

--- validarUser.js
+++ validarUser.js
@@ -19,8 +19,54 @@ formulario.addEventListener("submit", (e)=>{
 
 nombre.addEventListener("change",(e)=>{
     let dato = e.target.value.trim();
-    let expresion = /^[0-9]$/
-    if (dato.length < 1 || ){
-        
+    let expresion = /.[0-9]/
+    if (dato.length < 1 || expresion.test(dato)){
+        validar.nombre = false;
+    }
+    else{
+        validar.nombre = true;
     }
 })
+
+email.addEventListener("change",(e)=>{
+    let dato = e.target.value.trim();
+    let expresion = /.+@[a-z]+\.[a-z]{3}/;
+    if (dato.length > 12 && expresion.test(dato)){
+        validar.email = true;
+    }
+    else{
+        validar.email = false;
+    }
+})
+
+fecha.addEventListener("change",(e)=>{
+    let dato = new Date(e.target.value).getFullYear();
+    if (dato < 1920){
+        validar.fecha = false;
+    }
+    else{
+        validar.fecha = true;
+    }
+})
+
+dni.addEventListener("change", (e) => {
+    let dato = e.target.value.trim();
+    let expresion = /^[1-9][0-9]{7}[A-Z]$/;
+    if (dato.length == 9 || expresion.test(dato)){
+        validar.dni = true;
+    }
+    else{
+        validar.dni = false;
+    }
+})
+
+function validarFormulario (){
+    let datos = Object.values(validar);
+    let valido = datos.findIndex(valor => valor == false);
+    if (valido == -1) {
+        formulario.submit();
+    }
+    else{
+        alert("Formulario no valido\nCompruebe los datos");
+    }
+}
