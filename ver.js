340e6cfc9bcbce701a7c0e130855949ec2d2e486
Fran <franbl98@gmail.com>
2021-10-19 22:29:07 +0200

    Trabajando en comentarios y usuarios

--- ver.js
+++ ver.js
@@ -8,12 +8,47 @@ peticion.addEventListener("readystatechange",function(){
         if(peticion.status == 200){
             //Guardamos el array con todos los posts
             let datos = JSON.parse(peticion.responseText);
-            console.log(datos[0]);
             let cuerpo=document.getElementById("cuerpo");
-            let texto = document.createElement("p");
             let textoNode = document.createTextNode(datos[0].cuerpo);
-            texto.appendChild(textoNode);
-            cuerpo.appendChild(texto);
+            cuerpo.appendChild(textoNode);
+            let titulo=document.getElementById("titulo");
+            let textoNode2 = document.createTextNode(datos[0].title);
+            titulo.appendChild(textoNode2);
         }
     }
 })
+
+let peticion1 = new XMLHttpRequest();
+let id = window.location.search;
+peticion1.open("GET", `http://localhost:3000/users`);
+peticion1.send();
+//añadimos evento a la peticion
+peticion1.addEventListener("readystatechange",function(){
+    if(peticion1.readyState == 4){
+        if(peticion1.status == 200){
+           
+        }
+    }
+})
+
+let seccionComentaios = document.getElementById("comentarios");
+let peticion2 = new XMLHttpRequest();
+peticion2.open("GET", `http://localhost:3000/comments`);
+peticion2.send();
+//añadimos evento a la peticion
+peticion2.addEventListener("readystatechange",function(){
+    if(peticion2.readyState == 4){
+        if(peticion2.status == 200){
+            //Guardamos el array con todos los posts
+            let datos = JSON.parse(peticion.responseText);
+            datos.forEach(dato => {
+                if (dato.idPost == id){
+                    let texto = document.createTextNode(dato);
+                    seccionComentaios.appendChild(texto);
+                }
+            }); 
+        }
+    }
+})
+
+
